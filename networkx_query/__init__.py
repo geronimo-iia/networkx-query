cfcfb7c2ec84f34085eb4befbb351f2501500bfb
Jerome Guibert <jguibert@gmail.com>
2024-11-12 00:03:27 +0100

    fix: remove deprecated and unused version

--- networkx_query/__init__.py
+++ networkx_query/__init__.py
@@ -1,7 +1,5 @@
 """networkx-query."""
 
-from pkg_resources import DistributionNotFound, get_distribution
-
 from .definition import Evaluator, ParserException
 from .parser import prepare_query
 from .query import search_edges, search_nodes
@@ -17,8 +15,3 @@ __all__ = [
     'search_relationships',
     'PathCriteria',
 ]
-
-try:
-    __version__ = get_distribution('networkx_query').version
-except DistributionNotFound:  # pragma: no cover
-    __version__ = '(local)'
--- tests/test_version.py
+++
@@ -1,7 +0,0 @@
-"""Test unit for module version."""
-
-
-def test_version_is_defined():
-    from networkx_query import __version__
-
-    assert __version__
